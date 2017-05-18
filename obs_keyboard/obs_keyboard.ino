fb4c3e7b619977cf050a9c7955d2b98ddb645b51
Osiris Lopez <Osiris Lopez>
2017-05-17 22:55:44 -0700

    v1.0

---
+++ obs_keyboard/obs_keyboard.ino
@@ -0,0 +1,29 @@
+// OBS HotKeys arduino keyboard
+// Make by OsirisFrik
+// Twitter & Twitch OsirisFrik
+// Git https://github.com/OsirisFrik/OBS_HotKey_Arduino
+
+#include <Keyboard.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(3, INPUT_PULLUP);
+  pinMode(4, INPUT_PULLUP);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Keyboard.begin();
+  if (digitalRead (3) == 0){
+    Keyboard.press(0x80); // Left Ctrl
+    Keyboard.press('1'); // Num 1
+    delay(100); // Delay 100 ms
+    Keyboard.releaseAll(); // unpress all keys
+  }
+  else if (digitalRead (4) == 0){
+    Keyboard.press(0x80); // Left Ctrl
+    Keyboard.press('2'); // Num 1
+    delay(100); // Delay 100 ms
+    Keyboard.releaseAll(); // unpress all keys
+  }
+}
