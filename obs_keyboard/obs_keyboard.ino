05959348fa4a323f283f0f6c1a8ae2e043bece90
Osiris Lopez <Osiris Lopez>
2017-05-31 20:45:00 -0700

    nuevo codigo

--- obs_keyboard/obs_keyboard.ino
+++ obs_keyboard/obs_keyboard.ino
@@ -5,81 +5,89 @@
 
 #include <Keyboard.h>
 
-const int switchA = 2;
-int buttonA = 0;
-int buttonB = 0;
-int buttonC = 0;
+const int key1 = 4;
+const int led1 = 3;
+const int key2 = 2;
+const int led2 = 5;
+const int key3 = 6;
+const int led3 = 7;
+const int key4 = 8;
+const int led4 = 11;
+const int key5 = 10;
+const int led5 = 9;
 
-const int ledYellow = 12;
-const int ledGreen = 11;
-const int ledRed = 10;
-const int ledBlue = 9;
+const int led = 12;
 
-int state = 0;
-bool onAir = false;
-bool mode = true;
+int air = false;
+int mode = false;
+
+int press1 = 0;
+int press2 = 0;
+int press3 = 0;
+int press4 = 0;
+int press5 = 0;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(switchA, INPUT);
-  pinMode(3, INPUT_PULLUP);
-  pinMode(4, INPUT_PULLUP);
-  pinMode(5, INPUT_PULLUP);
+  pinMode(key1, INPUT_PULLUP);
+  pinMode(key2, INPUT_PULLUP);
+  pinMode(key3, INPUT_PULLUP);
+  pinMode(key4, INPUT_PULLUP);
+  pinMode(key5, INPUT_PULLUP);
 
-  pinMode(ledYellow, OUTPUT);
-  pinMode(ledGreen, OUTPUT);
-  pinMode(ledRed, OUTPUT);
-  pinMode(ledBlue, OUTPUT);
-  
+  pinMode(led, OUTPUT);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  pinMode(led5, OUTPUT);
+
   Keyboard.begin();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  state = digitalRead(switchA);
-  buttonA = digitalRead(3);
-  buttonB = digitalRead(4);
-  buttonC = digitalRead(5);
-
-  if (buttonA == 0){    
-    digitalWrite(ledBlue, HIGH);    
+  press1 = digitalRead(key1);
+  press2 = digitalRead(key2);
+  press3 = digitalRead(key3);
+  press4 = digitalRead(key4);
+  press5 = digitalRead(key5);
+  
+  if(press1 == 0){
+    if(air == false) {
+      air = true;
+      digitalWrite(led1, HIGH);
+    } else {
+      air = false;
+      digitalWrite(led1, LOW);
+    }
     Keyboard.press(0x80);
     Keyboard.press(0x81);
-    Keyboard.press('t');
-    if (onAir == true){
-      onAir = false;      
-      digitalWrite(ledRed, LOW);
-      delay(500);
-    } else {
-      onAir = true;      
-      digitalWrite(ledRed, HIGH);
-      delay(500);
-    }    
+    Keyboard.press('t');    
+    delay(120);
     Keyboard.release(0x80);
     Keyboard.release(0x81);
     Keyboard.release('t');
-    digitalWrite(ledBlue, LOW);
   }
 
-  if (state == 0) {
-    digitalWrite(ledGreen, HIGH);
-    digitalWrite(ledYellow, LOW);
-    delayMicroseconds(50);
-    mode = true;
-  } else {
-    digitalWrite(ledGreen, LOW);
-    digitalWrite(ledYellow, HIGH);
-    delay(100);
-    mode = false;
+  if(press2 == 0) {
+    if(mode == false) {
+      mode = true;
+      digitalWrite(led2, HIGH);
+    } else {
+      mode = false;
+      digitalWrite(led2, LOW);
+    }
+    delay(120);
   }
 
-  if (buttonB == 0){
-    digitalWrite(ledBlue, HIGH);
-    if (mode == false){
+  if(press3 == 0) {
+    digitalWrite(led3, HIGH);
+    if(mode == false) {
       Keyboard.press(0x80);
       Keyboard.press(0x81);
       Keyboard.press('1');
-      delay(200);
+      delay(120);
       Keyboard.release(0x80);
       Keyboard.release(0x81);
       Keyboard.release('1');
@@ -87,31 +95,55 @@ void loop() {
       Keyboard.press(0x80);
       Keyboard.press(0x81);
       Keyboard.press('m');
-      delay(200);
+      delay(120);
       Keyboard.release(0x80);
       Keyboard.release(0x81);
       Keyboard.release('m');
     }
-    digitalWrite(ledBlue, LOW);
-  } else if (buttonC == 0){
-    digitalWrite(ledBlue, HIGH);
-    if (mode == false){
+    digitalWrite(led3, LOW);
+  }
+
+  if(press4 == 0) {
+    digitalWrite(led4, HIGH);
+    if(mode == false) {
       Keyboard.press(0x80);
       Keyboard.press(0x81);
       Keyboard.press('2');
-      delay(200);
+      delay(120);
       Keyboard.release(0x80);
       Keyboard.release(0x81);
       Keyboard.release('2');
     } else {
       Keyboard.press(0x80);
       Keyboard.press(0x81);
-      Keyboard.press('b');
-      delay(200);
+      Keyboard.press('g');
+      delay(120);
       Keyboard.release(0x80);
       Keyboard.release(0x81);
-      Keyboard.release('b');
+      Keyboard.release('g');
     }
-    digitalWrite(ledBlue, LOW);
+    digitalWrite(led4, LOW);
+  }
+  
+  if(press5 == 0) {
+    digitalWrite(led5, HIGH);
+    if(mode == false) {
+      Keyboard.press(0x80);
+      Keyboard.press(0x81);
+      Keyboard.press('3');
+      delay(120);
+      Keyboard.release(0x80);
+      Keyboard.release(0x81);
+      Keyboard.release('3');
+    } else {
+      Keyboard.press(0x80);
+      Keyboard.press(0x81);
+      Keyboard.press('u');
+      delay(120);
+      Keyboard.release(0x80);
+      Keyboard.release(0x81);
+      Keyboard.release('u');
+    }
+    digitalWrite(led5, LOW);
   }
 }
